0215e0b377e655b095199a4c1cfa643ccc958958
Krzysztof Zielonka <aknolezik@gmail.com>
2019-01-14 22:42:14 +0100

    Add custom matcher for events

--- events/spec/creating_event_spec.rb
+++ events/spec/creating_event_spec.rb
@@ -1,6 +1,21 @@
 require "spec_helper"
 
 describe "creating event" do
+  matcher :have_events do |*expected|
+    match do |actual|
+      actual.all_events.collect(&:class) == expected.collect(&:class) &&
+        actual.all_events.collect(&:data) == expected.collect(&:data)
+    end
+
+    failure_message do |actual|
+      msg = "Expected events are:\n\n\n"
+      expected.each { |ev| msg += "\t#{ev.class}\n\t#{ev.data}\n\n" }
+      msg += "\nbut got:\n\n\n"
+      actual.all_events.each { |ev| msg += "\t#{ev.class}\n\t#{ev.data}\n\n" }
+      msg
+    end
+  end
+
   subject { Events.new(event_store) }
 
   let(:event_store) { EventStoreFake.new }
@@ -31,12 +46,12 @@ describe "creating event" do
         number_of_tickets: 5,
       ),
     )
-    expect(event_store.all_events.map(&:data)).to eql([
-      { id: event_id },
-      { id: event_id, title: "Title" },
-      { id: event_id, description: "Description" },
-      { id: event_id, number_of_tickets: 5 },
-    ])
+    expect(event_store).to have_events(
+      Events::DomainEvents::EventCreated.new(data: { id: event_id }),
+      Events::DomainEvents::EventTitleUpdated.new(data: { id: event_id, title: "Title" }),
+      Events::DomainEvents::EventDescriptionUpdated.new(data: { id: event_id, description: "Description" }),
+      Events::DomainEvents::EventNumberOfTicketsUpdated.new(data: { id: event_id, number_of_tickets: 5 }),
+    )
   end
 
   it "updates number of tickets" do
@@ -49,12 +64,12 @@ describe "creating event" do
         number_of_tickets: 10,
       ),
     )
-    expect(event_store.all_events.map(&:data)).to eql([
-      { id: event_id },
-      { id: event_id, title: "Title" },
-      { id: event_id, description: "Description" },
-      { id: event_id, number_of_tickets: 10 },
-    ])
+    expect(event_store).to have_events(
+      Events::DomainEvents::EventCreated.new(data: { id: event_id }),
+      Events::DomainEvents::EventTitleUpdated.new(data: { id: event_id, title: "Title" }),
+      Events::DomainEvents::EventDescriptionUpdated.new(data: { id: event_id, description: "Description" }),
+      Events::DomainEvents::EventNumberOfTicketsUpdated.new(data: { id: event_id, number_of_tickets: 10 }),
+    )
   end
 
   it "raises error trying update not created event" do
@@ -146,13 +161,13 @@ describe "creating event" do
       ),
     )
     subject.execute_command(Events::Commands::PublishEvent.new(event_id: event_id))
-    expect(event_store.all_events.map(&:data)).to eql([
-      { id: event_id },
-      { id: event_id, title: "Title" },
-      { id: event_id, description: "Description" },
-      { id: event_id, number_of_tickets: 10 },
-      { id: event_id },
-    ])
+    expect(event_store).to have_events(
+      Events::DomainEvents::EventCreated.new(data: { id: event_id }),
+      Events::DomainEvents::EventTitleUpdated.new(data: { id: event_id, title: "Title" }),
+      Events::DomainEvents::EventDescriptionUpdated.new(data: { id: event_id, description: "Description" }),
+      Events::DomainEvents::EventNumberOfTicketsUpdated.new(data: { id: event_id, number_of_tickets: 10 }),
+      Events::DomainEvents::EventPublished.new(data: { id: event_id }),
+    )
   end
 
   it "raises error trying to publish error with empty title" do
--- events/spec/spec_helper.rb
+++ events/spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require "events"
+require 'events'
 
 class EventStoreFake
   def initialize
@@ -23,6 +23,23 @@ class EventStoreFake
 
   def publish(events, options)
     @streams[String(options.fetch(:stream_name))].concat(events)
+  end
+end
+
+require 'rspec/expectations'
+
+RSpec::Matchers.define :have_events do |*expected|
+  match do |actual|
+    actual.all_events.collect(&:class) == expected.collect(&:class) &&
+      actual.all_events.collect(&:data) == expected.collect(&:data)
+  end
+
+  failure_message do |actual|
+    msg = "Expected events are:\n\n\n"
+    expected.each { |ev| msg += "\t#{ev.class}\n\t#{ev.data}\n\n" }
+    msg += "\nbut got:\n\n\n"
+    actual.all_events.each { |ev| msg += "\t#{ev.class}\n\t#{ev.data}\n\n" }
+    msg
   end
 end
 
